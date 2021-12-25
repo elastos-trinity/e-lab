dec963caf09856248bfeacc6fe0a185acae86632
Sofiane Sadi <sofiane.sadi@and.digital>
2021-12-25 12:11:19 +0000

    Sign in page and side menu completed

--- client-angular/src/app/pages/auth/pages/sign-in/sign-in.page.ts
+++ client-angular/src/app/pages/auth/pages/sign-in/sign-in.page.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
 import { ActivatedRoute, Router } from '@angular/router';
 import { ROUTER_UTILS } from '@core/utils/router.utils';
 import { AuthService } from '../../services/auth.service';
@@ -12,7 +12,7 @@ import { ElastosConnectivityService } from "@core/services/elastos-connectivity/
 /**
  * The sign-in page.
  */
-export class SignInPage {
+export class SignInPage implements OnInit {
   isLoggedIn!: boolean;
   isLoading = false;
 
@@ -21,29 +21,33 @@ export class SignInPage {
     private activatedRoute: ActivatedRoute,
     private elastosConnectivityService: ElastosConnectivityService,
     private authService: AuthService
-  ) {
+  ) { }
+
+  async ngOnInit(): Promise<void> {
     if (this.elastosConnectivityService.isAlreadyConnected() && this.authService.isLoggedIn$) {
-      this.elastosConnectivityService.restoreWalletSession().then(() => {
-        console.log("Wallet session restored")
-      })
+      await this.elastosConnectivityService.restoreWalletSession()
     }
     this.authService.isLoggedIn$.subscribe((v) => {
       this.isLoggedIn = v;
     })
   }
 
+
   async onClickSignIn(): Promise<void> {
-    // If the elastos connectivity session is already existing disconnecting first.
-    if (this.elastosConnectivityService.isAlreadyConnected()) {
-      await this.authService.signOut();
-      await this.authService.signIn();
-      await this.router.navigate([`/${ROUTER_UTILS.config.base.home}`]);
-      return Promise.resolve();
-    } else {
-      this.isLoading = true;
-      await this.authService.signIn();
-      await this.router.navigate([`/${ROUTER_UTILS.config.base.home}`]);
-      return Promise.resolve();
+    try {
+      if (this.elastosConnectivityService.isAlreadyConnected()) {
+        await this.authService.signOut();
+        await this.authService.signIn();
+        await this.router.navigate([`/${ROUTER_UTILS.config.base.home}`]);
+        return Promise.resolve();
+      } else {
+        this.isLoading = true;
+        await this.authService.signIn();
+        await this.router.navigate([`/${ROUTER_UTILS.config.base.home}`]);
+        return Promise.resolve();
+      }
+    } catch {
+      this.isLoading = false;
     }
   }
 }
